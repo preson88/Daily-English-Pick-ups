9d5e86aa11bf0c7527ed5bf1bf0504e216cd4ea0
preson88 <preson88@gmail.com>
2026-03-01 09:23:10 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
     title: "Daily English Pick Ups", // 기존에 있던 코드
-    description: "...",              // 기존에 있던 코드
+    description: "나만의 표현을 영어로 만드는 단어장",              // 기존에 있던 코드
     
     verification: {
       google: "cAHLDiRWplB0TJxA8SWNiVKxoHTNyDi_MPbrKdCKJmg", 
