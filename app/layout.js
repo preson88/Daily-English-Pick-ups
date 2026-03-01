793a4a72a7b472c41f4347306f4e6fa2c76a9ac5
preson88 <preson88@gmail.com>
2026-03-01 09:18:47 +0900

    Create layout.js
    구글 콘솔 소유권 확인 HTML 태그

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Daily English Pick Ups", // 기존에 있던 코드
+    description: "...",              // 기존에 있던 코드
+    
+    verification: {
+      google: "cAHLDiRWplB0TJxA8SWNiVKxoHTNyDi_MPbrKdCKJmg", 
+    },
+  };
